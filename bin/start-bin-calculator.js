a943120960a6f6d7e1b70de16ebc6f3c92769bac
Ozgur VURGUN <ozgurvurgun1@gmail.com>
2024-12-13 11:34:43 +0300

    first commit

---
+++ bin/start-bin-calculator.js
@@ -0,0 +1,80 @@
+const path = require("path");
+const fs = require("fs").promises;
+const BinCalculator = require(path.join(
+  __dirname,
+  "..",
+  "src",
+  "BinCalculator.js"
+));
+
+const dataFilePath = path.join(__dirname, "..", "data", "real-data.json");
+const outputFilePath = path.join(__dirname, "..", "output", "result-bin-analysis.csv");
+
+function congruencySelector(data, congruency) {
+  return data.filter((item) => item.congruency === congruency);
+}
+
+csvColumnHeaders = [
+  "part.code",
+  "bin1_cong",
+  "bin2_cong",
+  "bin3_cong",
+  "bin4_cong",
+  "bin5_cong",
+  "bin1_incong",
+  "bin2_incong",
+  "bin3_incong",
+  "bin4_incong",
+  "bin5_incong",
+];
+
+const csvHeaders = csvColumnHeaders.join(",") + "\n"; // Stringleri virgüllerle birleştirir
+
+async function run(start, end) {
+  try {
+    // Dosyayı oku
+    const fileContent = await fs.readFile(dataFilePath, "utf-8");
+    const data = JSON.parse(fileContent);
+    let csv = "";
+
+    for (let i = start; i <= end; i++) {
+      let filteredData = data.filter((item) => Number(item["part.code"]) == i);
+      if (filteredData.length === 0) {
+        continue;
+      }
+
+      const congruentData = congruencySelector(filteredData, "Congruent"); // Congruent OR Incongruent
+      if (congruentData.length === 0) {
+        continue;
+      }
+
+      const binCalculatorCongruent = new BinCalculator();
+      await binCalculatorCongruent.calculate(congruentData, 5);
+      csv += i +","+ binCalculatorCongruent
+        .getResult()
+        .map((item) => item["Total Reaction Time"])
+        .join(",");
+
+      const Incongruent = congruencySelector(filteredData, "Incongruent"); // Congruent OR Incongruent
+      if (Incongruent.length === 0) {
+        continue;
+      }
+
+      let binCalculatorIncongruent = new BinCalculator();
+      await binCalculatorIncongruent.calculate(Incongruent, 5);
+      csv +=
+        "," +
+        binCalculatorIncongruent
+          .getResult()
+          .map((item) => item["Total Reaction Time"])
+          .join(",") + "\n";
+    }
+    // console.log(csvHeaders + csv);
+  //  const result = csvHeaders + csv;
+    await fs.writeFile(outputFilePath,csvHeaders + csv, "utf-8");
+    console.log(`CSV başarıyla kaydedildi: ${outputFilePath}`);
+  } catch (error) {
+    console.error("Error processing the data:", error);
+  }
+}
+run(1, 24);
---
+++ src/BinCalculator.js
@@ -0,0 +1,222 @@
+const fs = require("fs/promises");
+
+class BinCalculator {
+  constructor() {
+    this.currentBin = {};
+    this.result = [];
+    this.dataLengthPossibilities = {
+     "5k+1" :   [0, 0, 0, 0, 0],
+     "5k+2" :   [0, 0, 1, 0, 0],
+     "5k+3" :   [0, 1, 0, 1, 0],
+     "5k+4" :   [0, 1, 1, 1, 0]
+    };
+    this.selectedDataLength = [];
+  }
+
+  // Utility: Round to nearest step
+  roundToNearestStep(num, step) {
+    return Math.ceil(num / step) * step;
+  }
+
+  // Display bins as a table
+  displayBinsTable() {
+    const tableData = [];
+
+    this.result.forEach((binData, index) => {
+      const binName = `bin${index + 1}`;
+      const binItems = binData[binName];
+
+      if (binItems && binItems.length > 0) {
+        const totalReactionTime = binItems.reduce((sum, item) => sum + item.binResult, 0);
+        const minReactionTime = Math.min(...binItems.map((item) => item.reaction_time));
+        const maxReactionTime = Math.max(...binItems.map((item) => item.reaction_time));
+
+        tableData.push({
+          Bin: binName,
+          "Total Reaction Time": totalReactionTime,
+          "Min Reaction Time": minReactionTime,
+          "Max Reaction Time": maxReactionTime,
+        });
+      }
+    });
+
+    console.table(tableData);
+  }
+
+  displayBinsJson(){
+    console.log(JSON.stringify(this.result, null, 2));
+  }
+
+  getResult(){
+    const tableData = [];
+
+    this.result.forEach((binData, index) => {
+      const binName = `bin${index + 1}`;
+      const binItems = binData[binName];
+
+      if (binItems && binItems.length > 0) {
+        const totalReactionTime = binItems.reduce((sum, item) => sum + item.binResult, 0);
+        const minReactionTime = Math.min(...binItems.map((item) => item.reaction_time));
+        const maxReactionTime = Math.max(...binItems.map((item) => item.reaction_time));
+
+        tableData.push({
+          Bin: binName,
+          "Total Reaction Time": totalReactionTime,
+          "Min Reaction Time": minReactionTime,
+          "Max Reaction Time": maxReactionTime,
+        });
+      }
+    });
+
+    return tableData
+  }
+
+  tempResultRecord(bin_name, binResult, reaction_time, numerator) {
+    let binObject = {
+      binResult: binResult,
+      reaction_time: reaction_time,
+      numerator: numerator,
+    };
+    this.currentBin[bin_name].push(binObject);
+  }
+
+  findClosestAddition(start, increment, target) {
+    let current = start;
+    let closestDifference = target - current;
+    let closestCount = 0;
+  
+    for (let count = 0; current <= target; count++) {
+      const newDifference = target - current;
+  
+      if (newDifference < closestDifference) {
+        closestDifference = newDifference;
+        closestCount = count;
+      }
+  
+      // Bir sonraki adıma geç
+      current += increment;
+    }
+  
+    return {closestDifference, closestCount};
+  }
+
+  // Core logic for bin calculation
+  calculateBins(sortedData, dataLength, realBinCount, innerIteration) {
+    let tempBin = realBinCount;
+    let tempBinCounter = 0;
+    let arrayCounter = 0;
+  
+    for (let i = 0; i < realBinCount; i++) {
+      this.currentBin = {};
+      this.currentBin[`bin${i + 1}`] = [];
+      let realIteration = innerIteration + this.selectedDataLength[i]
+      for (let j = 0; j < realIteration; j++) { 
+        
+        if (i === 0) {
+          tempBinCounter += realBinCount;
+          tempBin = tempBinCounter > dataLength ? dataLength % realBinCount : tempBin;
+          let binResult = (tempBin / dataLength) * sortedData[j].reaction_time;
+          this.tempResultRecord(`bin${i + 1}`, binResult, sortedData[j].reaction_time, tempBin);          
+ 
+        } else {
+          const previousBin = this.result[i - 1];
+          const binName = `bin${i}`;
+          let binLastElement;
+          let startNumerator = 0;
+          let startReactionTime = 0;
+          binLastElement = (previousBin && previousBin[binName].length > 0) ? previousBin[binName].at(-1) : undefined;
+
+          if (j === 0) {
+            startNumerator = realBinCount - binLastElement.numerator;
+            startReactionTime = binLastElement.reaction_time;
+            let binResult = (startNumerator / dataLength) * startReactionTime;
+            this.tempResultRecord(`bin${i + 1}`, binResult, startReactionTime, startNumerator);
+                        
+          } else if (j >= 1 && j <= realIteration - 2) {
+            let binResult = (realBinCount / dataLength) * sortedData[arrayCounter ].reaction_time;
+            this.tempResultRecord(`bin${i + 1}`, binResult, sortedData[arrayCounter].reaction_time, realBinCount);
+                      
+          } else if (j == realIteration - 1 && i != realBinCount - 1){
+            let numerator =  this.findClosestAddition(realBinCount - binLastElement.numerator, realBinCount, dataLength).closestDifference;            
+            let binResult = (numerator / dataLength) * sortedData[arrayCounter].reaction_time;
+            this.tempResultRecord(`bin${i + 1}`, binResult, sortedData[arrayCounter].reaction_time, numerator);
+        
+          } else{
+            let binResult = (realBinCount / dataLength) * sortedData[arrayCounter].reaction_time;
+            this.tempResultRecord(`bin${i + 1}`, binResult, sortedData[arrayCounter].reaction_time, realBinCount);
+           
+          }
+        }
+
+        arrayCounter++;
+      }
+
+      arrayCounter--;
+      this.result.push(this.currentBin);
+
+    }
+  }
+
+  calculateBinsExact(sortedData, dataLength, realBinCount, innerIteration){
+    let arrayCounter = -1;
+
+    for (let i = 0; i < realBinCount; i++) {
+      this.currentBin = {};
+      this.currentBin[`bin${i + 1}`] = [];
+      
+      for (let j = 0; j < innerIteration; j++) {
+        arrayCounter++;
+        let binResult = (realBinCount / dataLength) * sortedData[arrayCounter].reaction_time;
+        this.tempResultRecord(`bin${i + 1}`, binResult, sortedData[arrayCounter].reaction_time, realBinCount);
+      }
+
+      this.result.push(this.currentBin);
+    }
+  }
+
+  // Main bin calculation function
+async calculate(data, binCount) {
+  this.data = data;
+  this.binCount = binCount;
+  try {
+    // const jsonData = await fs.readFile(this.dataFile, "utf8");
+    // const data = JSON.parse(jsonData);
+
+    // Sort data by "congruency" first, then by "reaction_time"
+    // const sortedData = this.data.sort((a, b) => {
+    //   if (a.congruency === b.congruency) {
+    //     // If "congruency" values are equal, sort by "reaction_time"
+    //     return a.reaction_time - b.reaction_time;
+    //   }
+    //   // Prioritize "Congruent" over "Incongruent"
+    //   return a.congruency === "Congruent" ? -1 : 1;
+    // });
+
+    const dataLength = this.data.length;
+    const innerIteration = this.roundToNearestStep(dataLength, this.binCount) / this.binCount;
+    
+    if (dataLength % this.binCount == 0) {
+      this.calculateBinsExact(this.data, dataLength, this.binCount, innerIteration);
+    }else{
+
+      if (dataLength % this.binCount == 1) {
+        this.selectedDataLength = this.dataLengthPossibilities["5k+1"];
+      } else if(dataLength % this.binCount == 2){
+        this.selectedDataLength = this.dataLengthPossibilities["5k+2"];
+      }
+      else if(dataLength % this.binCount == 3){
+        this.selectedDataLength = this.dataLengthPossibilities["5k+3"];
+      } else if(dataLength % this.binCount == 4){
+        this.selectedDataLength = this.dataLengthPossibilities["5k+4"];
+      }
+      
+      this.calculateBins(this.data, dataLength, this.binCount, innerIteration);
+    }
+
+  } catch (err) {
+    console.error("Error reading file:", err);
+  }
+}
+}
+
+module.exports = BinCalculator;
